989193dbb81a255395c6ddc83c1da261188b1353
yasinok <yasinok@subu.edu.tr>
2024-06-28 16:57:04 +0300

    Entity ler tamamlandı.

---
+++ Core/CarBook.Domain/Entities/Service.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Domain.Entities
+{
+    public class Service
+    {
+        public int ServicesId { get; set; }
+
+        public string Title{ get; set; }
+        public string Descripton { get; set; }
+        public string IconUrl { get; set; }
+
+    }
+}
---
+++ Core/CarBook.Domain/Entities/Testimonial.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Domain.Entities
+{
+    public class Testimonial
+    {
+        public int TestimonialId { get; set; }
+        public string Name { get;}
+        public string Title { get;}
+        public string Commebt { get;}
+        public string ImageUrl { get;}
+        
+    }
+}
