6c76d3bd64c115b90b0bdda5c1e79149ea39da80
yasinok <yasinok@subu.edu.tr>
2024-07-05 11:38:29 +0300

    Migration çıkıldı. About için CQRS içerikleri yazıldı.

--- Presantation/CarBook.WebApi/obj/Debug/net8.0/CarBook.WebApi.AssemblyInfo.cs
+++ Presantation/CarBook.WebApi/obj/Debug/net8.0/CarBook.WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarBook.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+989193dbb81a255395c6ddc83c1da261188b1353")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce71e62637c46c7f1a8b20c8c70a673b96425d20")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarBook.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarBook.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
