f6143b11af680639a2d6578bac3f0689e7927b11
yasinok <yasinok@subu.edu.tr>
2024-08-01 17:13:14 +0300

    Tema sayfası dahil edildi. ViewComponent örneği yapıldı.(_HeadUILayoutComponentPartial)

---
+++ Frontends/CarBook.WebUI/ViewComponents/UILayoutViewComponent/_HeadUILayoutComponentPartial.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebUI.ViewComponents.UILayoutViewComponent
+{
+    public class _HeadUILayoutComponentPartial : ViewComponent
+    { 
+        public IViewComponentResult Invoke()
+        {
+
+            return View();
+        }
+
+    }
+}
