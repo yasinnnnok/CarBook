b97ced519d37161823623df4453e1475e81b11c0
yasinok <yasinok@subu.edu.tr>
2024-08-07 10:18:12 +0300

    About içeriği DB den getirildi.

--- Frontends/CarBook.WebUI/ViewComponents/AboutVieComponents/_AboutUsComponentPartial.cs
+++ Frontends/CarBook.WebUI/ViewComponents/AboutVieComponents/_AboutUsComponentPartial.cs
@@ -1,12 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Carbook.Dto.AboutDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace CarBook.WebUI.ViewComponents.AboutVieComponents
 {
-    public class _AboutUsComponentPartial:ViewComponent
+    public class _AboutUsComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _AboutUsComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7011/api/About");
+
+            //200,201-> başarılı döndü.
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultAboutDto>>(jsonData);
+                return View(values);
+            }
+
             return View();
         }
+        //public IViewComponentResult Invoke()
+        //{
+        //    return View();
+        //}
+
     }
 }
