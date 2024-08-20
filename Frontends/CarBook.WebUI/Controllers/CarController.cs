11c25baa4672212892330f19ceb9904a22d187e6
yasinok <yasinok@subu.edu.tr>
2024-08-20 15:51:29 +0300

    Araba Listesi geliyor. CarController.

---
+++ Frontends/CarBook.WebUI/Controllers/CarController.cs
@@ -0,0 +1,34 @@
+﻿using Carbook.Dto.CarDtos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.Controllers
+{
+    public class CarController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CarController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7011/api/Cars/GetCarWithBrand");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData= await responseMessage.Content.ReadAsStringAsync();
+                var values =JsonConvert.DeserializeObject<List<ResultCarWithBrandsDto>>(jsonData);
+                return View(values);
+                    
+            }
+
+            return View();
+        }
+    }
+}
