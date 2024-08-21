0527f72cee6f0768972245a9e390bf482b31bfec
yasinok <yasinok@subu.edu.tr>
2024-08-21 11:04:49 +0300

    Contact syfasındaki rnesaj gönderme işlemi tamamlandı.

--- Frontends/CarBook.WebUI/Controllers/ContactController.cs
+++ Frontends/CarBook.WebUI/Controllers/ContactController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Carbook.Dto.ContactDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Drawing.Printing;
+using System.Text;
 
 namespace CarBook.WebUI.Controllers
 {
@@ -16,5 +19,22 @@ namespace CarBook.WebUI.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateContactDto createContactDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            createContactDto.SenDate = DateTime.Now;
+            var jsonData = JsonConvert.SerializeObject(createContactDto);
+            StringContent stringContent=new StringContent(jsonData,Encoding.UTF8,"application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7011/api/Contacs",stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index","Default");
+            }
+            return View();
+             
+        }
+
     }
 }
