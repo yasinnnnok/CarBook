abec5b002e30bf5e866c60af8be0b8360f422934
yasinok <yasinok@subu.edu.tr>
2024-08-21 08:57:31 +0300

    Adres Bilgileri listelendi.

---
+++ Frontends/CarBook.WebUI/Controllers/ContactController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Drawing.Printing;
+
+namespace CarBook.WebUI.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ContactController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Frontends/CarBook.WebUI/ViewComponents/FooterAdressComponents/_FooterAdressComponentPartial.cs
@@ -0,0 +1,31 @@
+﻿using Carbook.Dto.FooterAdressDtos;
+
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.ViewComponents.FooterAdressComponents
+{
+    public class _FooterAdressComponentPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _FooterAdressComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7011/api/FooterAdress");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultFooterAdressDto>>(jsonData);
+                return View(values);
+            }
+
+            return View();
+        }
+    }
+}
